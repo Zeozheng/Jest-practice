086dc089b29d7c4b6ff9bda19a4e896f851db832
Zoezheng <zoe@m8buy.co>
2020-12-09 15:58:30 +1100

    Jest test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import './App.css';
+import Button from './components/button/button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +17,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <Button label = "click me"></Button>
       </header>
     </div>
   );
